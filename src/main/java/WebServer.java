65af9b41503f5812efd399c48e3229ce805210f6
Andrew <andrewmail404@gmail.com>
2025-11-29 16:17:14 -0800

    partial stuff, will work on later

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,20 +1,38 @@
 package com.andrew;
 
+import java.sql.*;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.ServerWebSocket;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 
-
 public class WebServer extends AbstractVerticle {
+	private HttpServer server;
+	private Router router;
+
   @Override
   public void start() {
-    HttpServer server = vertx.createHttpServer();
-    
-    Router router = Router.router(vertx);
-    router.route("/*").handler(StaticHandler.create("app"));
-    
-    server.requestHandler(router);
-    server.listen(8080);
+    this.server = vertx.createHttpServer();
+    this.server.webSocketHandler(ServerWebSocket ws -> {
+      if (!"/chats".equals(ws.path())) {
+        ws.reject();
+        return;
+      }
+      
+      ws.textMessageHandler(String message -> {
+        // implement later
+      });
+
+      ws.exceptionHandler(err -> {
+        System.err.println("WebSocket error: " + err.getMessage());
+      });
+    });
+
+    this.router = Router.router(vertx);
+    this.router.route("/*").handler(StaticHandler.create("app"));
+
+    this.server.requestHandler(router);
+    this.server.listen(8080);
   }
 }
