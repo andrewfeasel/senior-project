dbb29f07a2a4f2c01cc8cca6dff8f25f7c10a8fc
Andrew <andrewmail404@gmail.com>
2026-01-06 22:30:39 -0800

    fixed security vulnerability; added Mustache for a template engine

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 'use strict';
-const express = require("express");
-const sql = require("sqlite3").verbose();
-const path = require("node:path");
 
-/* uses djb2 */
+const express = require("express");
+const Database = require("better-sqlite3");
+const Mustache = require("mustache");
+const path = require("node:path");
+const fs = require("node:fs");
+
+const config = require("./config.json");
+
 function createHash(str) {
   let hash = 5381;
   for (let i = 0; i < str.length; i++) {
@@ -12,41 +16,44 @@ function createHash(str) {
   return hash;
 }
 
-const config = require("./config.json");
+/* SERVER INIT */
+process.chdir("dist");
 
-const db = new sql.Database(config.db_path, (error) => {
-	if (error) throw error;
-	console.log("sqlite3 connect ok");
-});
+const chat_template = fs.readFileSync(config.template_path, "utf8");
+Mustache.parse(chat_template);
 
-db.run("CREATE TABLE IF NOT EXISTS chats (ip_addr VARCHAR(48) NOT NULL, username VARCHAR(16) NOT NULL, message TEXT)");
+const db = new Database(config.db_path);
+db.pragma("journal_mode = WAL");
+db.exec("CREATE TABLE IF NOT EXISTS chats (ip_addr VARCHAR(48) NOT NULL, username VARCHAR(16) NOT NULL, message TEXT)");
+
+const db_insert = db.prepare("INSERT INTO chats (ip_addr, username, message) VALUES (?, ?, ?)");
 
 const chats = [];
 const server = express();
 
+/* REQUEST HANDLING */
 server.get("/chats", (req, res) => {
 	res.status(200);
-	res.set("Content-Type", "application/json");
-	res.json(chats);
+	res.set("Content-Type", "text/html");
+	res.send(chats.join(''));
 });
 
 
-server.use(express.json(config.express.json));
+server.use(express.urlencoded(config.express.urlencode));
 server.post("/chats", (req, res) => {
-	db.run("INSERT INTO chats(ip_addr, username, message) VALUES (?, ?, ?)",
-	[req.socket.remoteAddress, req.body.username, req.body.message], (error) => {
-		if (error) console.error(error);
-		const recv_message = {
-			message: req.body.message,
+	db_insert.run(req.socket.remoteAddress, req.body.username, req.body.message);
+	try {
+		const chat_html = Mustache.render(chat_template, {
+			hash: createHash(req.socket.remoteAddress),
 			username: req.body.username,
-			hash: createHash(req.socket.remoteAddress)
-		};
-		chats.push(recv_message);
-		if (chats.length > config.chat_limit) {
-			chats.shift();
-		}
+			message: req.body.message
+		});
+		chats.push(chat_html);
 		res.sendStatus(201);
-	});
+	} catch(e) {
+		console.error(e);
+		res.sendStatus(418);
+	}
 });
 
 const file_path = path.join(__dirname, config.app_path);
